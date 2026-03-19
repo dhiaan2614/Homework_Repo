2fbbc428a51b2f3a1b42ed4e6164e72acc8e3860
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-03-19 11:32:33 +0530

    Add files via upload

---
+++ class2hw.py
@@ -0,0 +1,12 @@
+rahul="19 october"
+sneha="21 march"
+rohan="29 february"
+suhani="11 june"
+devraj="2 december"
+print(rahul,sneha,rohan,suhani,devraj)
+
+swaroop="7 may" 
+roshni="12 march"
+dhiaan="26 march"
+caramel="23 june"
+print(swaroop,roshni,dhiaan,caramel)
