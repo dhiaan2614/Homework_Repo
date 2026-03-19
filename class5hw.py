881544e7633d8d7e8db07fe2c1932fafe981f120
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-03-19 11:34:05 +0530

    Add files via upload

---
+++ class5hw.py
@@ -0,0 +1,7 @@
+weather = float(input('Please enter the weather outside '))
+
+if weather >30 :
+    print('You do not need a jacket!')
+elif weather >20 and weather <30:
+    print('You may need a jacket')
+else :  print('You will need a jacket')
